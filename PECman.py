34203993bba4bb51587cc38d2eefb0488c199adc
NiCrook <nicrook@gmail.com>
2020-09-11 14:39:31 -0500

    New frames
    added 2 new frames, one being the RNG

--- PECman.py
+++ PECman.py
@@ -1,19 +1,57 @@
 import tkinter as tk
 from tkinter import IntVar
-
-"""
-A gui that has the user input their number of outs and the likelihood they believe their villian to fold and calculates
-their equity, fold equity, and total equity.
-"""
+from tkinter import StringVar
+import random
 
 
-class EquityWindow:
-    def __init__(self, master):
-        # define window
-        self.master = master
-        master.title('Poker Equity Calculator V1.2')
-        self.frame = tk.Frame(self.master)
-        self.frame.grid()
+# CONTAINER FRAME TO HOLD OTHER FRAMES
+class ContainerFrame(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # establish frame parameters
+        container = tk.Frame(self)
+        self.title("Poker Equity Calculator v1.21")
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        # sets each frame up with a name, parent, and controller
+        self.frames = {}
+        for F in (StartFrame, EquityFrame, RNGFrame):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("StartFrame")
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+# SPLASH FRAME
+class StartFrame(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        self.start_splash_text = tk.Label(self, text="PECman v2")
+
+        self.pec_button = tk.Button(self, text="PEC", command=lambda: controller.show_frame("EquityFrame"))
+        self.rng_button = tk.Button(self, text="RNG", command=lambda: controller.show_frame("RNGFrame"))
+
+        self.start_splash_text.grid(row=0, column=0)
+        self.pec_button.grid(row=1, column=0)
+        self.rng_button.grid(row=1, column=1)
+
+
+# EQUITY CALCULATOR FRAME
+class EquityFrame(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
 
         # VARIABLES
         # predefine variables for future use
@@ -23,7 +61,7 @@ class EquityWindow:
         self.fold_equity = 0
         self.total_equity = 0
 
-        # set variables to integer types and set their values
+        # set variables to integer types and set contents
         self.entered_outs_text = IntVar()
         self.entered_folds_text = IntVar()
         self.natural_equity_text = IntVar()
@@ -42,29 +80,29 @@ class EquityWindow:
 
         # WIDGETS
         self.labels = {
-            'outs_question': tk.Label(self.frame, text='How many outs do you have?'),
-            'outs_text': tk.Label(self.frame, text='Outs: '),
-            'entered_outs': tk.Label(self.frame, textvariable=self.entered_outs_text),
-            'equity_text': tk.Label(self.frame, text='Equity: '),
-            'equity_percent': tk.Label(self.frame, textvariable=self.natural_equity_text),
-            'fold_question': tk.Label(self.frame, text='How likely are they to fold?'),
-            'F%_text': tk.Label(self.frame, text='F%: '),
-            'entered_folds': tk.Label(self.frame, textvariable=self.entered_folds_text),
-            'fold_equity_text': tk.Label(self.frame, text='FE: '),
-            'fold_equity': tk.Label(self.frame, textvariable=self.fold_equity_text),
-            'total_equity_text': tk.Label(self.frame, text='Total: '),
-            'total_equity': tk.Label(self.frame, textvariable=self.total_equity_text)
+            'outs_question': tk.Label(self, text='How many outs do you have?'),
+            'outs_text': tk.Label(self, text='Outs: '),
+            'entered_outs': tk.Label(self, textvariable=self.entered_outs_text),
+            'equity_text': tk.Label(self, text='Equity: '),
+            'equity_percent': tk.Label(self, textvariable=self.natural_equity_text),
+            'fold_question': tk.Label(self, text='How likely are they to fold?'),
+            'F%_text': tk.Label(self, text='F%: '),
+            'entered_folds': tk.Label(self, textvariable=self.entered_folds_text),
+            'fold_equity_text': tk.Label(self, text='FE: '),
+            'fold_equity': tk.Label(self, textvariable=self.fold_equity_text),
+            'total_equity_text': tk.Label(self, text='Total: '),
+            'total_equity': tk.Label(self, textvariable=self.total_equity_text)
         }
 
         self.entries = {
-            'outs_entry': tk.Entry(self.frame, validate='key', validatecommand=(self.vcmd1, '%P')),
-            'folds_entry': tk.Entry(self.frame, validate='key', validatecommand=(self.vcmd2, '%P'))
+            'outs_entry': tk.Entry(self, validate='key', validatecommand=(self.vcmd1, '%P')),
+            'folds_entry': tk.Entry(self, validate='key', validatecommand=(self.vcmd2, '%P'))
         }
 
         self.buttons = {
-            'flop_button': tk.Button(self.frame, text='Flop', command=lambda: self.button_push('flop')),
-            'turn_button': tk.Button(self.frame, text='Turn', command=lambda: self.button_push('turn')),
-            'calculate_button': tk.Button(self.frame, text='Calculate', command=lambda: self.button_push('calculate'))
+            'flop_button': tk.Button(self, text='Flop', command=lambda: self.button_push('flop')),
+            'turn_button': tk.Button(self, text='Turn', command=lambda: self.button_push('turn')),
+            'calculate_button': tk.Button(self, text='Calculate', command=lambda: self.button_push('calculate'))
         }
 
         # LAYOUT
@@ -139,10 +177,116 @@ class EquityWindow:
         self.total_equity_text.set(self.total_equity)
 
 
+# RANDOM NUMBER GENERATOR FRAME
+class RNGFrame(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        # VARIABLES
+        # predefine variables for future use
+        self.outcome_text = None
+        self.entered_fold = 0
+        self.entered_call = 0
+        self.entered_bet = 0
+        self.entered_raise = 0
+
+        # set variable to string type and set contents
+        self.outcome_label_text = StringVar()
+        self.entered_fold = IntVar()
+        self.outcome_label_text.set(self.outcome_text)
+
+        # define validate command variable
+        self.vcmd1 = self.master.register(self.validate)
+
+        # WIDGETS
+        generate_button = tk.Button(self, text="Generate", command=lambda: self.generate('generate'))
+
+        entries = {
+            "fold_entry": tk.Entry(self, validate='key', validatecommand=(self.vcmd1, '%P')),
+            "call_entry": tk.Entry(self, validate='key', validatecommand=(self.vcmd1, '%P')),
+            "bet_entry": tk.Entry(self, validate='key', validatecommand=(self.vcmd1, '%P')),
+            "raise_entry": tk.Entry(self, validate='key', validatecommand=(self.vcmd1, '%P'))
+        }
+
+        self.labels = {
+            "RNG_label": tk.Label(self, text="Random Number Generator"),
+            "freq_label": tk.Label(self, text="Entered desired frequencies..."),
+            "fold_label": tk.Label(self, text="Fold: "),
+            "call_label": tk.Label(self, text="Call: "),
+            "bet_label": tk.Label(self, text="Bet: "),
+            "raise_label": tk.Label(self, text="Raise"),
+            "outcome_label": tk.Label(self, text="Outcome: "),
+            "outcome_result_label": tk.Label(self, textvariable=self.outcome_label_text)
+        }
+
+        # LAYOUT
+        self.labels['RNG_label'].grid(row=0, column=0, columnspan=5)
+        self.labels['freq_label'].grid(row=2, column=0, columnspan=4)
+        self.labels['fold_label'].grid(row=5, column=0, columnspan=1)
+        entries['fold_entry'].grid(row=5, column=1, columnspan=1)
+        self.labels['call_label'].grid(row=5, column=2, columnspan=1)
+        entries['call_entry'].grid(row=5, column=3, columnspan=1)
+        self.labels['bet_label'].grid(row=6, column=0, columnspan=1)
+        entries['bet_entry'].grid(row=6, column=1, columnspan=1)
+        self.labels['raise_label'].grid(row=6, column=2, columnspan=1)
+        entries['raise_entry'].grid(row=6, column=3, columnspan=1)
+        generate_button.grid(row=7, column=0, columnspan=1)
+        self.labels['outcome_label'].grid(row=7, column=2, columnspan=1)
+        # labels['outcome_result_label'].grid(row=7, column=3, columnspan=1)
+
+    # METHODS
+    def validate(self, new_char):
+        if not new_char:
+            self.entered_fold = 0
+            self.entered_call = 0
+            self.entered_bet = 0
+            self.entered_raise = 0
+            return True
+
+        try:
+            self.entered_fold = int(new_char)
+            self.entered_call = int(new_char)
+            self.entered_bet = int(new_char)
+            self.entered_raise = int(new_char)
+            return True
+        except ValueError:
+            return False
+
+    def generate(self, method):
+        # if method == "generate":
+        action_list = []
+        action_dict = {
+            "fold": str("Fold"),
+            "call": str("Call"),
+            "bet": str("Bet"),
+            "rraise": str("Raise")
+        }
+
+        if method == "generate":
+            for i in range(self.entered_fold):
+                action_list.append(action_dict['fold'])
+            for i in range(self.entered_call):
+                action_list.append(action_dict['call'])
+            for i in range(self.entered_bet):
+                action_list.append(action_dict['bet'])
+            for i in range(self.entered_raise):
+                action_list.append(action_dict['rraise'])
+
+            random.shuffle(action_list)
+            print(action_list)
+            self.outcome_text = action_list[0]
+            self.labels['outcome_result_label'].grid(row=7, column=3, columnspan=1)
+            # self.outcome_label_text.set(self.outcome_text)
+        else:
+            self.outcome_text = None
+
+        self.outcome_label_text.set(self.outcome_text)
+
+
 def main():
-    root = tk.Tk()
-    PEC_gui = EquityWindow(root)
-    root.mainloop()
+    PEC_gui = ContainerFrame()
+    PEC_gui.mainloop()
 
 
 if __name__ == '__main__':
